9ca619312149b1d630834fca5e3f4a7908d7057a
Xolisile Zwane <xolilez111@gmail.com>
2026-09-18 15:07:16 +0200

    created environment properties

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }]],
+  reporter: [['line'],
+    ['allure-playwright', { outputFolder: 'allure-results',detail: true,
+      suiteTitle: true, }]],
+    // ['html', { open: 'never', outputFolder: 'playwright-report' }]
+  
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
